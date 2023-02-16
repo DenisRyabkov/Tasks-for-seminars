924181a4c1e728bff170c7dad2eb7d203203dbf5
Denis <ryabkovdenis@yandex.ru>
2023-02-16 17:00:46 +0300

    added Task16

---
+++ Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа выводит случайное трехзначное число
+// и удаляет вторую цифру этого числа
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100-999 => {number}");
+
+int newDigit = NewDigit(number);
+Console.WriteLine($"Новое число => {newDigit}");
+
+int NewDigit(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+
+    return firstDigit * 10 + lastDigit;
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,23 @@
+﻿// программа принимает на вход два числа 
+// и выводит, является ли первое число кратным второму.
+// если не кратно, то выводит остаток от деления
+// 34,5 -> не кратно, остаток 4
+// 16,4 -> кратно
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+//if (number1 % number2 == 0) Console.WriteLine("Кратно");
+//else Console.WriteLine($"Не кратно, остаток {number1 % number2}");
+
+int result = Kratnost (number1, number2);
+
+if (result == 0) Console.WriteLine("Кратно");
+else Console.WriteLine ("Не кратно, остаток" + result);
+
+int Kratnost (int num1, int num2) 
+{
+    return num1 % num2;
+}
