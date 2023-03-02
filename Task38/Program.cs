8c7c0ab3b753742f41f738bc8e26d2780b9cac46
Denis <ryabkovdenis@yandex.ru>
2023-03-02 13:16:28 +0300

    added DZ39

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -2,7 +2,7 @@
 // Найдите разницу между максимальным и минимальным элементами массива
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
-double [] CreateArrayRndDouble(int size, int min, int max)
+double[] CreateArrayRndDouble(int size, int min, int max)
 {
     double[] arr = new double[size];
     Random rnd = new Random();
@@ -16,30 +16,56 @@ double [] CreateArrayRndDouble(int size, int min, int max)
 void PrintArrayDouble(double[] arr)
 {
     Console.Write("[");
-       for (int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        if(i< arr.Length - 1) Console.Write($"{arr[i]}, ");
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
         else Console.Write($"{arr[i]}");
-    } 
+    }
     Console.Write("]");
 }
 
-double GetMinNumber (double[] arr)
+double GetMaxNumber(double[] arr)
 {
-    double min = 0;
+    double max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+//double getMaxNumber = GetMaxNumber();
+GetMaxNumber();
+
+
+//double GetMinNumber (double[] arr)
+/*{
+    double min = arr [0];
+    minIndex = 0;
     for (int i = 0; i < arr.Length; i++)
 {
-	if (min > i) min = i;	
+	if (min > arr [i])
+    {
+        min = arr [i];
+        minIndex = i;
+    }
 }
 return min;
-}
+}*/
 
-double GetMaxNumber (double[] arr)
+/*double GetMaxNumber (double[] arr)
 {
-    double max = 0;
+    double max = arr [0];
+    maxIndex = 0;
     for (int i = 0; i < arr.Length; i++)
 {
-	if (max < i) max = i;
+	if (max < arr[i])
+    {
+       max = arr [i];
+       maxIndex = i; 
+    }
 }
 return max;
 }
@@ -52,14 +78,14 @@ double GetResultNumber (double[] arr)
 }
 return result;
 }
-
-double [] array = CreateArrayRndDouble(5, 1, 5);
+*/
+double[] array = CreateArrayRndDouble(5, 1, 5);
 PrintArrayDouble(array);
 Console.WriteLine();
 
-double getMinNumber = GetMinNumber(array);
-double getMaxNumber = GetMaxNumber(array);
-double getResultNumber = GetResultNumber(array);
+//double getMinNumber = GetMinNumber(array);
+//double getMaxNumber = GetMaxNumber(array);
+//double getResultNumber = GetResultNumber(array);
 //Console.WriteLine($"минимальное число = {getMinNumber}");
 //Console.WriteLine($"максимальное число = {getMaxNumber}");
-Console.WriteLine($" разница чисел = {getResultNumber}");
+//Console.WriteLine($" разница чисел = {getResultNumber}");
